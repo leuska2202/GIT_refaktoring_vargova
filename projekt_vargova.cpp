b6c08dc4bf2a8af6faa369aa489f9e3823296a26
Lea Vargova <leuska2202@azet.sk>
2018-12-11 21:29:43 +0100

    nacitanie vsetkych suborov zoznamu

--- projekt_vargova.cpp
+++ projekt_vargova.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include<stdlib.h>
+#include <stdlib.h>
 
 //STRUKTURA ZOZNAMU
 typedef struct	auta
@@ -14,20 +14,19 @@ typedef struct	auta
 	struct auta *dalsi;
 } AUTA;
 
-//NACITANIE SUBOROV
+//NACITANIE ZOZNAMU
 void nacitanie(AUTA **p_prvy, int *n) //n - smernik na hodnotu pocet_prvkov zoznamu p_prvy - smernik na pvu polozku zoznamu
 {
 	FILE * fp;
+	AUTA *prvy, *akt;
 	char c[2]; // nacitanie dolarovej znacky
-
-	AUTA *prvy;
 	int pocet_prvkov = 1; //na zaciatku bude mat 1. zaznam hodnotu 1
 
 	if ((fp = fopen("auta.txt", "r")) == NULL)
-		printf("Zaznamy neboli nacitane\n"); //ak sa subor nepodarilo otvorit
+		printf("Zaznamy neboli nacitane\n"); //ak sa subor nepodarilo otvorit;
 
 	prvy = (AUTA *)malloc(sizeof(AUTA)); //nacitanie prveho prvku zaznamu
-	prvy->hodnota = pocet_prvkov++;  //pocet_prvkov sa zvacsi 
+	prvy->hodnota = pocet_prvkov++;  //pocet_prvkov sa zvacsi aby dalsia nacitana hodnota bola 2
 	fscanf(fp, " %s", c);
 	fscanf(fp, " %[^\n]", prvy->kategoria);
 	fscanf(fp, " %[^\n]", prvy->znacka);
@@ -36,8 +35,27 @@ void nacitanie(AUTA **p_prvy, int *n) //n - smernik na hodnotu pocet_prvkov zozn
 	fscanf(fp, " %int", &prvy->rok_vyroby);
 	fscanf(fp, " %[^\n]", prvy->stav_vozidla);
 
+	akt = prvy; //aktualny prvok na zaciatku je aj prvy
+	while (!feof(fp))
+	{
+		akt->dalsi = (AUTA *)malloc(sizeof(AUTA));
+		akt = akt->dalsi; //presun na dalsi prvok
+
+		akt->hodnota = pocet_prvkov++; //nacitanie poloziek dalsieho prvku, kt. je teraz aktualny
+		fscanf(fp, " %[^\n]", c); // precita dollar na zaciatku zaznamu
+		fscanf(fp, " %[^\n]", akt->kategoria);
+		fscanf(fp, " %[^\n]", akt->znacka);
+		fscanf(fp, " %[^\n]", akt->predajca);
+		fscanf(fp, " %int", &akt->cena);
+		fscanf(fp, " %int", &akt->rok_vyroby);
+		fscanf(fp, " %[^\n]", akt->stav_vozidla);
+	}
+	akt->dalsi = NULL; //ukoncenie zoznamu 
+
 	if (fclose(fp) == EOF)
 		printf("Subor sa nepodarilo zatvorit\n");
+
+	printf("Nacitalo sa %d zaznamov. \n", pocet_prvkov - 1); 
 
 	*p_prvy = prvy; //odoslanie zoznamu do mainu 
 	*n = pocet_prvkov; // odoslanie poctu prvkov zoznamu do mainu
