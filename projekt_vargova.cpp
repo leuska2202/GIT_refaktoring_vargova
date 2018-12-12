7f62c9fa923794f57ceabc41894f2e46efdb173f
Lea Vargova <leuska2202@azet.sk>
2018-12-12 18:00:19 +0100

    vylepsenie a refakt. mainu aj s funkciami

--- projekt_vargova.cpp
+++ projekt_vargova.cpp
@@ -15,44 +15,44 @@ typedef struct	auta
 } AUTA;
 
 //NACITANIE ZOZNAMU
-void nacitanie(AUTA *prvy, int pocet_prvkov, AUTA **p_prvy, int *p_pocet_prvkov) //n - smernik na hodnotu pocet_prvkov zoznamu p_prvy - smernik na pvu polozku zoznamu
+void nacitanie(AUTA **p_prvy, int *p_pocet_prvkov) 
 {
-	FILE * fp;
-	AUTA *akt;
+	FILE * f_subor;
+	AUTA *prvy, *akt;
+	int pocet_prvkov = 1; //na zaciatku bude mat 1. zaznam hodnotu 1
 	char c[2]; // nacitanie dolarovej znacky
-	pocet_prvkov = 1; //na zaciatku bude mat 1. zaznam hodnotu 1
 
-	if ((fp = fopen("auta.txt", "r")) == NULL)
+	if ((f_subor = fopen("auta.txt", "r")) == NULL)
 		printf("Zaznamy neboli nacitane\n"); //ak sa subor nepodarilo otvorit;
 
 	prvy = (AUTA *)malloc(sizeof(AUTA)); //nacitanie prveho prvku zaznamu
 	prvy->hodnota = pocet_prvkov++;  //pocet_prvkov sa zvacsi aby dalsia nacitana hodnota bola 2
-	fscanf(fp, " %s", c);
-	fscanf(fp, " %[^\n]", prvy->kategoria);
-	fscanf(fp, " %[^\n]", prvy->znacka);
-	fscanf(fp, " %[^\n]", prvy->predajca);
-	fscanf(fp, " %int", &prvy->cena);
-	fscanf(fp, " %int", &prvy->rok_vyroby);
-	fscanf(fp, " %[^\n]", prvy->stav_vozidla);
+	fscanf(f_subor, " %s", c);
+	fscanf(f_subor, " %[^\n]", prvy->kategoria);
+	fscanf(f_subor, " %[^\n]", prvy->znacka);
+	fscanf(f_subor, " %[^\n]", prvy->predajca);
+	fscanf(f_subor, " %int", &prvy->cena);
+	fscanf(f_subor, " %int", &prvy->rok_vyroby);
+	fscanf(f_subor, " %[^\n]", prvy->stav_vozidla);
 
 	akt = prvy; //aktualny prvok na zaciatku je aj prvy
-	while (!feof(fp))
+	while (!feof(f_subor))
 	{
 		akt->dalsi = (AUTA *)malloc(sizeof(AUTA));
 		akt = akt->dalsi; //presun na dalsi prvok
 
 		akt->hodnota = pocet_prvkov++; //nacitanie poloziek dalsieho prvku, kt. je teraz aktualny
-		fscanf(fp, " %[^\n]", c); // precita dollar na zaciatku zaznamu
-		fscanf(fp, " %[^\n]", akt->kategoria);
-		fscanf(fp, " %[^\n]", akt->znacka);
-		fscanf(fp, " %[^\n]", akt->predajca);
-		fscanf(fp, " %int", &akt->cena);
-		fscanf(fp, " %int", &akt->rok_vyroby);
-		fscanf(fp, " %[^\n]", akt->stav_vozidla);
+		fscanf(f_subor, " %[^\n]", c); // precita dollar na zaciatku zaznamu
+		fscanf(f_subor, " %[^\n]", akt->kategoria);
+		fscanf(f_subor, " %[^\n]", akt->znacka);
+		fscanf(f_subor, " %[^\n]", akt->predajca);
+		fscanf(f_subor, " %int", &akt->cena);
+		fscanf(f_subor, " %int", &akt->rok_vyroby);
+		fscanf(f_subor, " %[^\n]", akt->stav_vozidla);
 	}
 	akt->dalsi = NULL; //ukoncenie zoznamu 
 
-	if (fclose(fp) == EOF)
+	if (fclose(f_subor) == EOF)
 		printf("Subor sa nepodarilo zatvorit\n");
 
 	printf("Nacitalo sa %d zaznamov. \n \n", pocet_prvkov - 1); 
@@ -115,7 +115,7 @@ void pridanie(AUTA *prvy, int pocet_prvkov, AUTA **novy) // novy - pointer na no
 	
 	pridany->hodnota = miesto_vlozenia;
 	
-	printf("Kategoria auta: \n"); 
+	printf("\nKategoria auta: \n"); 
 	scanf(" %[^\n]", pridany->kategoria);
 	
 	printf("Znacka auta: \n");
@@ -157,7 +157,7 @@ void pridanie(AUTA *prvy, int pocet_prvkov, AUTA **novy) // novy - pointer na no
 		akt = akt->dalsi;
 	}
 
-	printf("Prvok pridany \n \n");
+	printf("\nPrvok pridany- ak chces vidiet zoznam zadaj 'v'. \n \n");
 
 	*novy = prvy; //vratenie noveho zoznamu do mainu
 }
@@ -169,29 +169,28 @@ int main()
 	p_prvy = NULL; //na zaciatku prazdny zoznam
 	int pocet_prvkov = 1; //pocet prvkov zoznamu
 
-	while (1)
+	printf("Zadavaj funkciu, kt. chces vykonat: \n('n'- nacitanie prvkov suboru / 'v'- vypis / 'p'- pridanie prvku / 'k'- koniec) \n");
+
+	while (1) //nekonecny cyklus
 	{
 		char funkcia;
+		
 		scanf("%c", &funkcia); // nacitanie funcie, kt. chceme vykonat
 
 		if (funkcia == 'k') break; //ukonci program - nekonecny cyklus 
 		
-		else if (funkcia == 'n')
+		switch (funkcia)
 		{
-			if (p_prvy != NULL) //ak zoznam existuje - ma uz nejake prvky uvolni existujuci zoznam a nacita novy
+		case 'n':
+			if (p_prvy != NULL) //ak zoznam existuje - ma prvky uvolni existujuci zoznam a nacita novy
 				uvolni(p_prvy, pocet_prvkov, &p_prvy);
 
 			p_prvy = (AUTA *)malloc(sizeof(AUTA));  
-			nacitanie(p_prvy, pocet_prvkov, &p_prvy, &pocet_prvkov);
-		}
+			nacitanie(&p_prvy, &pocet_prvkov); break; // alokacia pred nacitanim zoznamu a jeho nacitanie
 
-		else if (funkcia == 'v')
-			vypis(p_prvy, pocet_prvkov);
-
-		else if (funkcia == 'p')
-		{
-			pridanie(p_prvy, pocet_prvkov, &p_prvy); 
-			pocet_prvkov++;  //pridali sme prvok --> zvacsil sa nam zoznam o 1 --> pocet prvkov +1
+		case 'v': vypis(p_prvy, pocet_prvkov); break;
+		
+		case 'p': pridanie(p_prvy, pocet_prvkov, &p_prvy); pocet_prvkov++; break;  //pridali sme prvok --> zvacsil sa nam zoznam o 1 --> pocet prvkov +1
 		}
 	}
 
